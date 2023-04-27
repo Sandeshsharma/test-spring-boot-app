f2ba784bd044561119bbb27d85ef249f1c946eba
Sandeshsharma <sharma.sandu@gmail.com>
2023-04-27 12:24:18 -0400

    Update DemoApplication.java
    Added print statement

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Notice the Change");
         SpringApplication.run(DemoApplication.class, args);
     }
 
