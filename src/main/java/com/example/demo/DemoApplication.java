265a8a9515746ee615516e9e1bdd711d3b1948f2
Sandeshsharma <sharma.sandu@gmail.com>
2023-04-27 12:58:24 -0400

    Changing Main
    Change in Main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Notice the Change");
+        System.out.println("Change in main");
         SpringApplication.run(DemoApplication.class, args);
     }
 
